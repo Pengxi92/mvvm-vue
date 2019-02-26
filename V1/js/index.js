f5a992c002f52d115d306c7d385b066db9d2e91c
xi.peng <xi.peng@wormpex.com>
2019-02-26 15:41:19 +0800

    针对data属性添加代理

--- V1/js/index.js
+++ V1/js/index.js
@@ -6,11 +6,30 @@
  */
 function SelfVue (data, el, exp) {
   this.data = data;
+  var self = this;
+  Object.keys(data).forEach(function(key) {
+    self.proxyKeys(key); // 不直接使用this，应该这里this指向Object.keys(data)
+  });
   observe(data);
-  el.innerHTML = this.data[exp];  // 初始化模板数据的值
+  el.innerHTML = this[exp];  // 初始化模板数据的值
 
   // 添加属性的监听操作
   new Watcher(this, exp, function(value, oldVal) {
     el.innerHTML = value;
   })
+}
+
+// 针对data属性添加代理
+SelfVue.prototype.proxyKeys = function(key) {
+  var self = this;
+  Object.defineProperty(self, key, {
+    enumerable: false,
+    configurable: true,
+    set: function(val) {
+      self.data[key] = val;
+    },
+    get: function() {
+      return self.data[key];
+    },
+  });
 }
